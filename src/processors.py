81b368ddf97196e1f46a8326d2454593b93c6c68
Seth Steen-Fuentes <sethsteen-fuentes@Seths-MBP.attlocal.net>
2024-12-02 15:54:10 -0800

    refactor: remove processing route strategy classes behavior directly into document processor class.

--- src/processors.py
+++ src/processors.py
@@ -7,6 +7,7 @@ to ingesting files from the local filesystem.
 """
 
 import hashlib
+import uuid
 import warnings
 import logging
 from io import BytesIO
@@ -16,12 +17,15 @@ import os
 
 from dotenv import load_dotenv
 import paperqa as pqa
+from paperqa.utils import ImpossibleParsingError
 from paperqa.readers import chunk_pdf as pqa_chunk_pdf
 from paperqa.readers import parse_pdf_to_pages as pqa_parse_pdf_to_pages
 from paperqa.types import ParsedText, ParsedMetadata, ChunkMetadata, Text
+import pymupdf
 import tiktoken
 
 from src.adapters.vectordb import VectorStore
+from src.models import Document
 
 # Configure logging to suppress paper-qa's API-related messages
 logging.getLogger("paper_qa").setLevel(logging.INFO)
@@ -45,53 +49,6 @@ local_llm_config = dict(
         )
     ]
 )
-
-
-class ProcessingRoute:
-    """Interface for processing routes strategies."""
-
-    def process_document(self, input, **kwargs):
-        """Process a document."""
-        pass
-
-    def process_documents(self, input, **kwargs):
-        """Process a list of documents."""
-        pass
-
-    def chunk_pdf(self, text: Any, **kwargs) -> list[str]:
-        """Chunk a PDF document into chunks of a given size."""
-        pass
-
-    def extract_metadata(self, text: Any, **kwargs) -> Any:
-        """Extract metadata from text."""
-        pass
-
-
-class PQAProcessingRoute(ProcessingRoute):
-    """Process documents using paper-qa."""
-
-    def __init__(self, pqa_settings: Optional[pqa.Settings] = None):
-        self._pqa_settings = pqa_settings
-
-    def process_document( self, input: os.PathLike):
-        doc =
-
-    def process_documents(self, input: List[Union[os.PathLike, bytes, str, BytesIO]]):
-        pass
-
-    def chunk_pdf(
-        self, input: Union[os.PathLike, bytes, str, BytesIO], chunk_chars: int, overlap: int
-    ) -> list[Text]:
-
-        if isinstance(input, os.PathLike):
-            parsed_text: ParsedText = pqa_parse_pdf_to_pages(input)
-        elif isinstance(input, (bytes, str, BytesIO)):
-
-        return pqa_chunk_pdf(parsed_text, chunk_chars, overlap)
-
-
-    def extract_metadata(self, text: ParsedText, **kwargs) -> ParsedMetadata:
-        pass
 
 
 class DocumentProcessor:
@@ -158,3 +115,91 @@ class PQADocumentProcessor(DocumentProcessor):
         """
 
        pass
+
+
+    def process_documents(self, input: List[Union[os.PathLike, bytes, str, BytesIO]]):
+        pass
+
+    @staticmethod
+    def chunk_pdf(
+        input: Union[os.PathLike, bytes, str, BytesIO], chunk_chars: int, overlap: int
+    ) -> list[Text]:
+        """
+        Chunk a PDF document into chunks of a given size. Uses paper-qa's chunk_pdf
+        function for the actual chunking, but pagination is handled by either paperqa or
+        custom logic depending on the input type.
+        """
+
+        if isinstance(input, (os.PathLike, str)):
+            parsed_text = pqa_parse_pdf_to_pages(input)
+        elif isinstance(input, (bytes, BytesIO)):
+            parsed_text = PQAProcessingRoute.parse_pdf_bytes_to_pages(input)
+        else:
+            raise ValueError(f"Unsupported input type: {type(input)}")
+
+        return pqa_chunk_pdf(parsed_text, chunk_chars, overlap)
+
+    @staticmethod
+    def parse_pdf_bytes_to_pages(
+        input: Union[bytes, BytesIO], page_size_limit: int | None = None
+    ) -> ParsedText:
+        """
+        Parse a PDF within a byte stream the `ParsedText` paperqa object.
+
+        Nearly identical to paper-qa's parse_pdf_to_pages function, but handles bytes
+        instead of a file path.
+
+        Parameters
+        ----------
+        input : Union[bytes, BytesIO]
+            Byte stream of a PDF document.
+
+        Returns
+        -------
+        ParsedText
+            Paper-qa object containing the parsed PDF document.
+        """
+
+        with pymupdf.open(stream=input, filetype="pdf") as pdf:
+            pages: dict[int, str] = {}
+            total_length = 0
+
+            for i in range(pdf.page_count):
+                try:
+                    page = pdf.load_page(i)
+                except pymupdf.mupdf.FzErrorFormat as exc:
+                    raise ImpossibleParsingError(
+                    f"Page loading via {pymupdf.__name__} failed on page {i} of"
+                        f" {pdf.page_count} for the PDF bytes stream."
+                    ) from exc
+                text = page.get_text("text", sort=True)
+                if page_size_limit and len(text) > page_size_limit:
+                    raise ImpossibleParsingError(
+                        f"The text in page {i} of {pdf.page_count} was {len(text)} chars"
+                        f" long, which exceeds the {page_size_limit} char limit for the PDF"
+                        f" bytes stream."
+                    )
+                pages[str(i + 1)] = text
+                total_length += len(text)
+
+        metadata = ParsedMetadata(
+            parsing_libraries=[f"pymupdf {pymupdf.__version__}"],
+            paperqa_version=pqa.__version__,
+            total_parsed_text_length=total_length,
+            parse_type="pdf_bytes",
+        )
+
+        return ParsedText(pages, metadata)
+
+
+    def extract_metadata(
+        self, text: ParsedText, doc: Optional[Document] = None, **kwargs
+    ) -> Document:
+
+        if doc is None:
+            doc = Document(
+                id=str(uuid.uuid4()),
+            )
+
+
+
