6b5cda374d4abfea51171ae4a73b737db5f476e7
Seth Steen-Fuentes <steenfuentes@gmail.com>
2024-12-02 20:22:13 -0800

    chore: removed old test files. working on testing of vectorstore package.

---
+++ tests/test_src/test_vectorstores/test_vectordb.py
@@ -0,0 +1,9 @@
+import pytest
+from dataclasses import dataclass
+from src.vectorstores.keymanager import RedisKeyManager
+from src.vectorstores.vectordb import LCRedisVectorStore
+
+
+@pytest.fixture
+def vector_db():
+    return LCRedisVectorStore(redis_url="redis://localhost:6379", key_prefix="doc")
