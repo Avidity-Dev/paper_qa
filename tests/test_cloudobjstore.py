370e7e7b0bdad75d2cd3c231f1dccc4bc12a862a
Seth Steen-Fuentes <steenfuentes@gmail.com>
2024-12-09 15:49:07 -0800

    feat: updated azure blob client to support async context manager protocol.

--- tests/test_cloudobjstore.py
+++ tests/test_cloudobjstore.py
@@ -4,7 +4,7 @@ import os
 import dotenv
 import pytest
 
-from src.storage.object.stores import AzureBlobAdapter
+from src.storage.object.stores import AzureBlobStore
 
 dotenv.load_dotenv()
 
@@ -12,28 +12,41 @@ dotenv.load_dotenv()
 @pytest.mark.asyncio
 async def test_azure_blob_connection():
     blob_connection_string = os.getenv("AZURE_BLOB_CONNECTION_STRING")
-    blob_adapter = AzureBlobAdapter(blob_connection_string, "papers")
-    await blob_adapter._ensure_client()
-    assert blob_adapter.blob_service_client is not None
-    assert blob_adapter.container_client is not None
+    blob_store = AzureBlobStore(blob_connection_string, "papers")
+    async with blob_store as blob_store:
+        assert blob_store.blob_service_client is not None
+        assert blob_store.container_client is not None
 
 
 @pytest.mark.asyncio
 async def test_azure_blob_upload_and_delete():
     blob_connection_string = os.getenv("AZURE_BLOB_CONNECTION_STRING")
-    blob_adapter = AzureBlobAdapter(blob_connection_string, "test")
-    await blob_adapter._ensure_client()
-    await blob_adapter.upload_file(b"test", "test.txt")
+    blob_store = AzureBlobStore(blob_connection_string, "test")
+    async with blob_store as blob_store:
+        await blob_store.upload_file(b"test", "test.txt")
 
-    assert await blob_adapter.container_client.get_blob_client("test.txt").exists()
-    assert await blob_adapter.delete_file("test.txt")
+        assert await blob_store.container_client.get_blob_client("test.txt").exists()
+        assert await blob_store.delete_file("test.txt")
 
 
 @pytest.mark.asyncio
 async def test_azure_blob_list_files():
     blob_connection_string = os.getenv("AZURE_BLOB_CONNECTION_STRING")
-    blob_adapter = AzureBlobAdapter(blob_connection_string, "papers")
-    await blob_adapter._ensure_client()
-    files = await blob_adapter.list_files()
-    assert len(files) > 0
-    assert "AttentionIsAllYouNeed.pdf" in files
+    blob_store = AzureBlobStore(blob_connection_string, "papers")
+    async with blob_store as blob_store:
+        files = await blob_store.list_files()
+        assert len(files) > 0
+        assert "AttentionIsAllYouNeed.pdf" in [file.name for file in files]
+
+
+@pytest.mark.asyncio
+async def test_azure_blob_download_files():
+    blob_connection_string = os.getenv("AZURE_BLOB_CONNECTION_STRING")
+    blob_store = AzureBlobStore(blob_connection_string, "papers")
+    async with blob_store as blob_store:
+        files = await blob_store.list_files()
+        assert len(files) > 0
+        # just get first few files
+        file_stream = await blob_store.download_file(files[0])
+        assert file_stream is not None
+        assert file_stream.getvalue() is not None
